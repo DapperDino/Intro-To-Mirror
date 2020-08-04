fdca2afe0758437b83daf6a661d1931d5bc5e410
Nathan <natemi1010@gmail.com>
2020-08-04 14:34:18 +0100

    6_UYM Authority & Replication

---
+++ Assets/Scripts/MyNetworkPlayer.cs
@@ -0,0 +1,44 @@
+﻿using Mirror;
+using UnityEngine;
+
+namespace DapperDino.Multiplayer
+{
+    public class MyNetworkPlayer : NetworkBehaviour
+    {
+        [SyncVar(hook = nameof(ClientHandleDisplayNameUpdated))]
+        private string displayName;
+
+        #region Server
+
+        [Command]
+        private void CmdDoSomething()
+        {
+            // Server authority checks here
+
+            displayName = "Random Name";
+        }
+
+        #endregion
+
+        #region Client
+
+        [ClientCallback]
+        private void Update()
+        {
+            if (!hasAuthority) { return; }
+
+            if (!Input.GetKeyDown(KeyCode.Space)) { return; }
+
+            CmdDoSomething();
+        }
+
+        [Client]
+        private void ClientHandleDisplayNameUpdated(string oldDisplayName, string newDisplayName)
+        {
+            // Update UI
+        }
+
+        #endregion
+    }
+}
+
