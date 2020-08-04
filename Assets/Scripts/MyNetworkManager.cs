ccb475c0cb56fa44ecab2c146d91a9a255683687
Nathan <natemi1010@gmail.com>
2020-08-04 14:17:14 +0100

    4_UYM Connecting Two Players

---
+++ Assets/Scripts/MyNetworkManager.cs
@@ -0,0 +1,13 @@
+﻿using Mirror;
+using UnityEngine;
+
+namespace DapperDino.Multiplayer
+{
+    public class MyNetworkManager : NetworkManager
+    {
+        public override void OnServerConnect(NetworkConnection conn)
+        {
+            Debug.Log($"Player {conn.connectionId} has connected");
+        }
+    }
+}
